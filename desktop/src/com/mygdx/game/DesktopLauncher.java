4259c120cee248bf863b820a1150495226d96e4b
leonardo-guglielmi <leonardo.guglielmi@stud.unifi.it>
2023-02-22 11:49:10 +0100

    Added some configurations to game window

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -4,12 +4,17 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.PlumberParadise;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		// creating configuration for the game window
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
+		config.setIdleFPS(30);
 		config.setTitle("Plumber Paradise");
+		config.setMaximized(true);
+		config.setWindowIcon("plumberParadiseLogo.png");
+
+		//starting window application
 		new Lwjgl3Application(new PlumberParadise(), config);
 	}
 }
