c88e0318c7ef8786c146ba6e367eaf22973dacb2
leonardo-guglielmi <leonardo.guglielmi@stud.unifi.it>
2023-02-22 12:16:11 +0100

    Add some comments and modified indentation (some random tests)

--- core/src/com/mygdx/game/PlumberParadise.java
+++ core/src/com/mygdx/game/PlumberParadise.java
@@ -2,24 +2,26 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
+
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class PlumberParadise extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private SpriteBatch batch;
+	private Texture img;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		//loads the test image
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 1, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 100, 200);
 		batch.end();
 	}
 	
